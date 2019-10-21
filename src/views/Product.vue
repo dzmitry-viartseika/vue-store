017180c8036aa06e9cb5e6e41fbab4278d7f3015
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2019-10-21 11:55:43 +0300

    productItem component

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,15 +1,38 @@
-<template>
-  <div class="cart">
-    <h1>product</h1>
-  </div>
+<template lang="pug">
+  .app-item
+    .app-container
+      .app-item-row
+        .app-item-row__promo
+          img(:src="")
+        .app-item-row__info
+          .app-item-row__info-title Apple iPhone 7 Plus 32 GB (Apple Türkiye Garantili)
+          .app-item-row__info-price $4,241.50
+          .app-item-row__info-description Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit. Amet Numquam Aspernatur!
+          .app-item-row__info-brand Apple
+          .app-item-row__info-size 124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)
+          .app-item-row__info-camera 8mp (3264x2448)
+          .app-item-row__info-cpu 1.3GHz Apple A6
+          .app-item-row__info-memory 16GB, 32GB and RAM 1 GB
+          .app-item-row__info-display 4.0 326 pixel density
+          .app-item-row__info-battery 1480 mAh
+
+
 </template>
 
 <script>
-export default {
-  name: 'Product',
-};
+    export default {
+        name: 'ProductItem',
+    };
 </script>
 
 <style scoped lang="scss">
+  @import "../assets/scss/core.scss";
 
+  .app-item {
+    width: 100%;
+    height: 100%;
+    &-row {
+      border: 1px solid $borderColor;
+    }
+  }
 </style>
